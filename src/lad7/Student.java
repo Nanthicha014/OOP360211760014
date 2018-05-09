969b510df90d88af769a8e203391af20d9dd76b2
Nanthicha <Nanthicha.041239@gmail.com>
2018-05-09 12:00:31 +0700

    myArray

--- src/lad7/Student.java
+++ src/lad7/Student.java
@@ -4,4 +4,51 @@ package lad7;
 //3.gender
 //4.height
 public class Student {
-}
+    //properties of student class
+    private String name;
+    private int age;
+    private String gender;
+    private double height;
+    //default constructor
+    public Student(){}
+    //default constructor
+
+    public Student(String name, int age, String gender, double height) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}//class
