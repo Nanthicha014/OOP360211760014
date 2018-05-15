8845aacfbd8a598b71b8ad9fac773b19e1f57363
Nanthicha <Nanthicha.041239@gmail.com>
2018-05-15 16:44:51 +0700

    sutdentAppArrayList

---
+++ src/lad8/Dog.java
@@ -0,0 +1,13 @@
+package lab8;
+
+public class Dog extends Pet{
+
+
+    @Override
+    public void makeNoise(){
+        System.out.println("Box !!!");
+
+    }
+
+
+}
---
+++ src/lad8/cat.java
@@ -0,0 +1,10 @@
+package lad8;
+
+public class cat extends Pet {
+
+    @Override
+    public void makeNoise() {
+        System.out.println("");
+
+    }
+}
