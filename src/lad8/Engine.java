0dbf1d81bf668ac9045eeeab150bd001a30a36e7
Nanthicha <Nanthicha.041239@gmail.com>
2018-05-16 09:54:21 +0700

    Pet

---
+++ src/lad8/Engine.java
@@ -0,0 +1,20 @@
+package lad8;
+//1.engine size
+//2.horse power
+public class Engine {
+    private int engSize;
+    private int horsePower;
+
+    //constructor
+    public void Engine()()
+
+    public Engine(int engSize, int horsePower) {
+        this.engSize = engSize;
+        this.horsePower = horsePower;
+    }
+    //toString
+
+
+
+
+}//class
